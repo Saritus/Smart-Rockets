41b46c6fb7e2852b15e64e620b797ffcc42bd7d0
Sebastian Mischke <SebastianMischke@gmx.de>
2016-12-10 15:03:21 +0100

    Add applyForce

--- rocket.js
+++ rocket.js
@@ -2,4 +2,8 @@ function Rocket() {
   this.pos = createVector();
   this.vel = createVector();
   this.acc = createVector();
+
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
 }
